08cc87181229f39e5ae768cdc46c81260cd89e84
Christopher Siewert <christopher.siewert@live.ca>
2019-03-16 07:38:19 -0700

    Problem 2

--- bank.js
+++ bank.js
@@ -2,14 +2,30 @@
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
 
+var log = [
+    {cmd: 'deposit', amount: 130},
+    {cmd: 'deposit', amount: 0},
+    {cmd: 'deposit', amount: 120}
+];
+
+var getBalance = () => {
+    return log.reduce((sum, current) => sum + parseInt(current.amount), 0);
+};
+    
 var server = net.createServer(function (socket) {
     socket = jsonStream(socket)
 
     socket.on('data', function (msg) {
         console.log('Bank received:', msg)
-	socket.write({cmd: 'balance', balance: 0});    
-
-        // socket.write can be used to send a reply
+        switch (msg.cmd) {
+            case 'balance':
+                socket.write({cmd: 'balance', balance: getBalance()});
+                break;
+            case 'deposit':
+                log.push(msg);
+                socket.write(msg);
+                break;
+        }
     })
 })
 
--- teller.js
+++ teller.js
@@ -8,8 +8,16 @@ client.on('data', function (msg) {
     console.log('Teller received:', msg)
 })
 
-// client.end can be used to send a request and close the socket
+switch (process.argv[2]) {
+    case 'balance':
+        client.end({cmd: 'balance'}); 
+    	break;
+    case 'deposit':
+	client.end({cmd: 'deposit', amount: process.argv[3]});
+    	break;
+    default:
+	break;		
+}
 
-client.end({cmd: 'balance'});
 
 
