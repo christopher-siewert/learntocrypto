ace4dd5a287eacdfa6c76ee805302d881a22efca
Christopher Siewert <christopher.siewert@live.ca>
2019-03-17 08:49:46 -0700

    Problem 9

--- bank.js
+++ bank.js
@@ -4,16 +4,40 @@ const net = require('net')
 const fs = require('fs')
 const sodium = require('sodium-native');
 
+// genesis hash to use for hash chain
 const genesisHash = Buffer.alloc(32).toString('hex');
 
-var log = JSON.parse(fs.readFileSync("log.txt")); 
+// Try loading a public/private key pair from keys.txt
+// All variables in try catch are declared var so they are global
+try {
 
+    var keyPair = JSON.parse(fs.readFileSync('keys.txt'));
+    var publicKey = Buffer.from(keyPair.publicKey, 'hex');
+    var secretKey = Buffer.from(keyPair.secretKey, 'hex');
+
+} catch { // If keys.txt doesn't exist, create a new key pair and store to file
+    
+    var publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES);
+    var secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES);
+    sodium.crypto_sign_keypair(publicKey, secretKey);
+    var keyPair = {
+        secretKey: secretKey.toString('hex'), 
+        publicKey: publicKey.toString('hex')
+    }
+    fs.writeFileSync('keys.txt', JSON.stringify(keyPair, null, 2))
+}
+
+// Read transaction history
+let log = JSON.parse(fs.readFileSync("log.txt")); 
+
+// If the hash chain isn't correct exit immediately
 if (!verifyLog()) {
     console.log("The transaction log has been modified.");
     process.exit(1);
 }
 
-var server = net.createServer(function (socket) {
+// Server code
+let server = net.createServer(function (socket) {
     socket = jsonStream(socket)
     socket.on('data', function (msg) {
         console.log('Bank received:', msg)
@@ -34,12 +58,13 @@ var server = net.createServer(function (socket) {
                 }
                 break;
         }
-        fs.writeFileSync("log.txt", JSON.stringify(log, null, 4))
+        fs.writeFileSync("log.txt", JSON.stringify(log, null, 2))
     })
 })
 
 server.listen(3876);
 
+// Returns the hash of an input in hex
 function hashToHex(string) {
     let buf1 = Buffer.from(string);
     let buf2 = Buffer.alloc(sodium.crypto_generichash_BYTES);
@@ -47,33 +72,64 @@ function hashToHex(string) {
     return buf2.toString('hex');
 }
 
+// Loops through transaction log and totals up balance
 function getBalance() {
     return log.reduce((sum, entry) => {
+        // Add all deposits and minus all withdraws 
         if (entry.value.cmd == 'deposit') {
             return sum + parseInt(entry.value.amount);
         } else if (entry.value.cmd == 'withdraw') {
             return sum - parseInt(entry.value.amount); 
         }
-    }, 0)
+    }, 0) // 0 at end is to set initial sum to 0
 }
 
-
+// Adds entry to log array
 function appendToLog(entry) {
-    var prevHash = log.length ? log[log.length - 1].hash : genesisHash;
+    // Set prevhash to last entry's hash unless the log is empty
+    // If empty set to genesis hash
+    let prevHash = log.length ? log[log.length - 1].hash : genesisHash;
+    
+    // Calc current chain hash
+    let currentHash = hashToHex(prevHash + JSON.stringify(entry));
+
+    // Sign hash using secret key
+    let message = Buffer.from(currentHash, 'hex');
+    let signature = Buffer.alloc(sodium.crypto_sign_BYTES);
+    sodium.crypto_sign_detached(signature, message, secretKey)
+
+    // Push the entry, chained hash and signature of hash
     log.push({
         value: entry,
-        hash: hashToHex(prevHash + JSON.stringify(entry))
+        hash: currentHash,
+        signature: signature.toString('hex')
     });
 }
 
+// Returns true if hash chain is unbroken and all sigs match
 function verifyLog() {
-    var verify = true
-    var prevHash = genesisHash
-    for (var i = 0; i < log.length; i++) {
+    let verify = true
+    let prevHash = genesisHash
+    
+    // For each log entry
+    for (let i = 0; i < log.length; i++) {
+        
+        // If the calculated entry hash doesn't match the file set false
         if (hashToHex(prevHash + JSON.stringify(log[i].value)) != log[i].hash) {
             verify = false
         }
+        
+        // Set prevHash for next loop
         prevHash = log[i].hash
+
+        // Buffers for sig verify
+        let message = Buffer.from(log[i].hash, 'hex');
+        let signature = Buffer.from(log[i].signature, 'hex');
+        
+        // If signature doesn't verify set false
+        if (!sodium.crypto_sign_verify_detached(signature, message, publicKey)) {
+            verify = false
+        }
     }
     return verify;
 }
--- sign.js
+++ sign.js
@@ -1,12 +1,26 @@
 // sign.js 
 const sodium = require('sodium-native');
-
+const fs = require('fs');
 let message = Buffer.from(process.argv[2]);
-let publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES);
-let secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES);
-let signature = Buffer.alloc(sodium.crypto_sign_BYTES);
 
-sodium.crypto_sign_keypair(publicKey, secretKey);
+try {
+    var keyPair = JSON.parse(fs.readFileSync('keys.txt'));
+    var publicKey = Buffer.from(keyPair.publicKey, 'hex');
+    var secretKey = Buffer.from(keyPair.secretKey, 'hex');
+
+} catch {
+    var publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES);
+    var secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES);
+    sodium.crypto_sign_keypair(publicKey, secretKey);
+    var keyPair = {
+        secretKey: secretKey.toString('hex'), 
+        publicKey: publicKey.toString('hex')
+    }
+    fs.writeFileSync('keys.txt', JSON.stringify(keyPair, null, 2))
+}
+
+
+let signature = Buffer.alloc(sodium.crypto_sign_BYTES);
 
 sodium.crypto_sign_detached(signature, message, secretKey)
 
