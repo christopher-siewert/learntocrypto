a369a4b1bf12b05567e122a4506083d3ac57104a
Christopher Siewert <christopher.siewert@live.ca>
2019-03-17 06:44:57 -0700

    Problem 7

--- bank.js
+++ bank.js
@@ -1,23 +1,20 @@
 // bank.js
-var jsonStream = require('duplex-json-stream')
-var net = require('net')
+const jsonStream = require('duplex-json-stream')
+const net = require('net')
 const fs = require('fs')
+const sodium = require('sodium-native');
+
+const genesisHash = Buffer.alloc(32).toString('hex');
 
 var log = JSON.parse(fs.readFileSync("log.txt")); 
 
-var getBalance = () => {
-    return log.reduce((sum, entry) => {
-        if (entry.cmd == 'deposit') {
-            return sum + parseInt(entry.amount);
-        } else if (entry.cmd == 'withdraw') {
-            return sum - parseInt(entry.amount); 
-        }
-    }, 0)
-};
-    
+if (!verifyLog()) {
+    console.log("The transaction log has been modified.");
+    process.exit(1);
+}
+
 var server = net.createServer(function (socket) {
     socket = jsonStream(socket)
-
     socket.on('data', function (msg) {
         console.log('Bank received:', msg)
         switch (msg.cmd) {
@@ -25,12 +22,12 @@ var server = net.createServer(function (socket) {
                 socket.write({cmd: 'balance', balance: getBalance()});
                 break;
             case 'deposit':
-                log.push(msg);
+                appendToLog(msg);
                 socket.write(msg);
                 break;
             case 'withdraw':
-                if (getBalance() > msg.amount) {
-                    log.push(msg);
+                if (getBalance() >= msg.amount) {
+                    appendToLog(msg);
                     socket.write(msg);
                 } else {
                     socket.write("Insufficient funds.");
@@ -43,3 +40,40 @@ var server = net.createServer(function (socket) {
 
 server.listen(3876);
 
+function hashToHex(string) {
+    let buf1 = Buffer.from(string);
+    let buf2 = Buffer.alloc(sodium.crypto_generichash_BYTES);
+    sodium.crypto_generichash(buf2, buf1);
+    return buf2.toString('hex');
+}
+
+function getBalance() {
+    return log.reduce((sum, entry) => {
+        if (entry.value.cmd == 'deposit') {
+            return sum + parseInt(entry.value.amount);
+        } else if (entry.value.cmd == 'withdraw') {
+            return sum - parseInt(entry.value.amount); 
+        }
+    }, 0)
+}
+
+
+function appendToLog(entry) {
+    var prevHash = log.length ? log[log.length - 1].hash : genesisHash;
+    log.push({
+        value: entry,
+        hash: hashToHex(prevHash + JSON.stringify(entry))
+    });
+}
+
+function verifyLog() {
+    var verify = true
+    var prevHash = genesisHash
+    for (var i = 0; i < log.length; i++) {
+        if (hashToHex(prevHash + JSON.stringify(log[i].value)) != log[i].hash) {
+            verify = false
+        }
+        prevHash = log[i].hash
+    }
+    return verify;
+}
